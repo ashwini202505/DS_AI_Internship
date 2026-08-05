077d88f150b79ac74da06e5c482b22b30284c0f5
ashwini202505 <aashwinim989@gmail.com>
2026-08-05 12:30:57 +0530

    1st msg

---
+++ day2/lists.py
@@ -0,0 +1,6 @@
+list =[["name","Akash"],["age",21],["marks",[23,44,55]]]
+# print(list[2][1][1])
+
+print(list[0][0],":",list[0][1])
+print(list[1][0],":",list[1][1])
+print(list[2][0],":",list[2][1])
