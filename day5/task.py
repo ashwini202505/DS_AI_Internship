6f102c54f54f459c71f5c4fdbeeb3d9e3892ec54
ashwini202505 <aashwinim989@gmail.com>
2026-08-10 14:31:21 +0530

    msg1

--- day5/task.py
+++ day5/task.py
@@ -1,3 +1,10 @@
+prices = [100, 250, 80, 150, 300]
+updated_prices = []
+for price in prices:
+    updated_prices.append(price + 20)
+print("Original Prices:", prices)
+print("Updated Prices:", updated_prices)
+
 import numpy as np
 prices = np.array([100, 250, 80, 150, 300])
 updated_prices = prices + 20
